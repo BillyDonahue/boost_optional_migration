47c8086bc6b247c808f233bd072a68bbfa512008
Billy Donahue <billy.donahue@gmail.com>
2021-12-09 01:34:04 -0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,13 @@ mkdir -p build
 
 LLVM_INSTALL_DIR="${HOME}/prog/mongodb/llvm-project/build/install"
 LLVM_CONFIG_EXECUTABLE="${LLVM_INSTALL_DIR}/bin/llvm-config"
-# -D LLVM_CONFIG_EXECUTABLE="${LLVM_CONFIG_EXECUTABLE}"
 
 cmd=(
         cmake
-        -G "Unix Makefiles"
         -S .
-        -C build
+        -B build
         -D LLVM_DIR="${LLVM_INSTALL_DIR}/lib/cmake/llvm"
         -D Clang_DIR="${LLVM_INSTALL_DIR}/lib/cmake/clang"
+        -D LLVM_CONFIG_EXECUTABLE="${LLVM_CONFIG_EXECUTABLE}"
 )
 "${cmd[@]}"
