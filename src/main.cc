aa6cd67f38e5d8b333efa1a040f1f02994ada42c
Billy Donahue <billy.donahue@gmail.com>
2021-12-09 21:13:53 -0500

    tweak main

--- src/main.cc
+++ src/main.cc
@@ -1,21 +1,24 @@
 #include <string>
 
-#include <llvm/Support/CommandLine.h>
+#include <clang/Frontend/FrontendActions.h>
 #include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
 
 #include "actions/frontendaction.h"
 #include "utils/utils.h"
 
-int main(int argc, const char **argv)
-{
-    llvm::cl::OptionCategory ctCategory("clang-tool options");
-    auto opts = clang::tooling::CommonOptionsParser::create(argc, argv, ctCategory);
-    if (!opts) {
-        llvm::errs() << "cmdline option parse failed:" << opts.takeError();
-        return -1;
-    }
+static llvm::cl::OptionCategory category("boost-optional-migration options");
 
-    for (auto &src : opts->getSourcePathList()) {
+static const char* progname;
+
+static int revisedMain(clang::tooling::CommonOptionsParser& opts) {
+    clang::tooling::ClangTool tool(opts.getCompilations(), opts.getSourcePathList());
+    return tool.run(clang::tooling::newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+}
+
+static int originalMain(clang::tooling::CommonOptionsParser& opts) {
+    for (auto &src : opts.getSourcePathList()) {
         if (!utils::fileExists(src)) {
             llvm::errs() << "File: " << src << " does not exist!\n";
             return -1;
@@ -24,8 +27,8 @@ int main(int argc, const char **argv)
         auto absPath = clang::tooling::getAbsolutePath(src);
         llvm::outs() << "src='" << src << "', absPath='" << absPath << "'\n";
 
-        auto compileArgs = utils::getCompileArgs(opts->getCompilations().getCompileCommands(absPath));
-        compileArgs.push_back("-I" + utils::getClangBuiltInIncludePath(argv[0]));
+        auto compileArgs = utils::getCompileArgs(opts.getCompilations().getCompileCommands(absPath));
+        compileArgs.push_back("-I" + utils::getClangBuiltInIncludePath(progname));
 
         {
             llvm::outs() << "compileArgs: [\n";
@@ -40,6 +43,16 @@ int main(int argc, const char **argv)
             compileArgs,
             src);
     }
-
     return 0;
+
+}
+
+int main(int argc, const char **argv) {
+    auto opts = clang::tooling::CommonOptionsParser::create(argc, argv, category);
+    if (!opts) {
+        llvm::errs() << "cmdline option parse failed:" << opts.takeError();
+        return -1;
+    }
+    progname = argv[0];
+    return revisedMain(*opts);
 }
